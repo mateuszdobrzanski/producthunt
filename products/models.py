47c32c33fff024d207fa2992c1a328f00f2e052b
Mateusz Dobrzanski <mateuszdobrzasnki@outlook.com>
2019-07-23 17:51:10 +0200

    upload the project

---
+++ products/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField(default="")
+    url = models.TextField(default="")
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @staticmethod
+    def get_index_of_last_space(text):
+        index = 0
+        pom = 0
+
+        for i in text:
+            if i.isspace():
+                index = pom
+            pom += 1
+        return index
+
+    # in this function return summary
+    # for "fancy" look last chars at the and are "..."
+    def summary(self):
+        text_summary = self.body[:100]
+        index = self.get_index_of_last_space(text_summary)
+
+        if index > 98:
+            string = text_summary[0:index - 1]
+            index = self.get_index_of_last_space(string)
+            text_summary = string[0:index]
+            string = text_summary + "..."
+
+        else:
+            string = text_summary[0:index]
+            string += "..."
+
+        return string
+
+    # customize date and time
+    def pub_date_pretty(self):
+        return self.pub_type.strftime('%B %e %Y, %H:%M')
+
+    def votes_total(self):
+        votes = 0
+        for upvote in Upvote.objects.all():
+            if upvote.product == self:
+                votes += 1
+        return votes
+
+
+class Upvote(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ products/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product, Upvote
+from django.utils import timezone
+
+
+def home(request):
+    products = Product.objects.all()
+    return render(request,
+                  'products/home.html',
+                  {'products': products})
+
+
+@login_required(login_url="/accounts/login")
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+
+            # checking if url starts with https or http prefix
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+
+            # set current time on promotion
+            product.pub_date = timezone.datetime.now()
+
+            # get current username for occasion
+            product.hunter = request.user
+
+            # -----save to database
+            product.save()
+
+            # return user to the product detail web page
+            return redirect('/products/' + str(product.id))
+        else:
+            return render(request, 'products/create.html', {'error': 'All fields are required'})
+    else:
+        return render(request, 'products/create.html')
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
+
+
+@login_required(login_url="/accounts/login")
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        upvotes = Upvote.objects.all()
+        for upvote in upvotes:
+            if upvote.product == product and upvote.voter == request.user:
+                return redirect('/products/' + str(product.id))
+        # will only get her if there is no match inside the loop
+        new_upvote = Upvote()
+        new_upvote.product = product
+        new_upvote.voter = request.user
+        new_upvote.save()
+        return redirect('/products/' + str(product.id))
